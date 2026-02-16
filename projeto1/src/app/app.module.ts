ded24e11b4c1bc82df567b53807f4141c9f35e42
Cargnieli-2016 <cargnieli2016@gmail.com>
2026-02-16 14:25:56 -0300

    adicionado o componente cabeçalho, conteúdo e rodape

--- projeto1/src/app/app.module.ts
+++ projeto1/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { ConteudoComponent } from './componentes/conteudo/conteudo.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    ConteudoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
