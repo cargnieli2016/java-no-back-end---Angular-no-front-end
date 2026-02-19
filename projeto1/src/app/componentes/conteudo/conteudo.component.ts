0821d33de316d4ee86f4e09e1efb89d9bf2d9276
Cargnieli-2016 <cargnieli2016@gmail.com>
2026-02-19 08:51:16 -0300

    ciclo de vida, interpolação, event e property binding

--- projeto1/src/app/componentes/conteudo/conteudo.component.ts
+++ projeto1/src/app/componentes/conteudo/conteudo.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { tick } from '@angular/core/testing';
 
 @Component({
   selector: 'app-conteudo',
   templateUrl: './conteudo.component.html',
   styleUrls: ['./conteudo.component.css']
 })
-export class ConteudoComponent {
+export class ConteudoComponent implements OnInit {
+
+  public mensagem: string;
+  public valor: number;
+  public link_foto: string;
+
+  public constructor() {
+    this.mensagem = 'Olá mundo!';
+    this.valor = 0;
+    this.link_foto = '/assets/isiflix.jpg';
+  }
+
+  ngOnInit(): void {
+
+
+  }
+
+  public mudarValor() {
+    this.valor++;
+    this.link_foto = "/assets/images.jpg";
+    //console.log(this.valor);
+  }
 
 }
